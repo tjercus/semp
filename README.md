7d6043b89bb8bb7c106f9a0bd75c48cf200ed811
tvalentijn <tjerk.valentijn@visma.com>
2022-05-23 16:17:12 +0200

    hook up mqtt

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # SEMP
 
 Smart Energy Monitor Publisher. 
-Reads the data from the meter and publishes it via MQTT
+Reads the data from the meter and publishes it via MQTT.
+You need an MQTT broker (in your LAN or Cloud) to handle the data.
+
+## configuring
+
+You need some environment variables:
+- MQTT_BROKER_URL="mqtt://my.broker.com/"
+
+create a file called .env (dot env) and put the envs (keys + values) in it.
+
+## running
+
+### run a local test:
+- start your MQTT broker on your LAN
+- put the broker URL in the .env file
+- npm run start
+
+### run via a systemd demon 
+
+(Ubuntu, Rapsberry PI OS etc.)
+
+see: https://nodesource.com/blog/running-your-node-js-app-with-systemd-part-1/
--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
+require("dotenv").config();
 const SmartMeter = require("node-dsmr");
-// const mqtt = require("mqtt");
+const mqtt = require("mqtt");
 
 const smartMeterOptions = {
-  port: "/dev/ttyUSB0",
-  baudrate: 115200,
+  port: "/dev/ttyUSB1",
+  baudrate: 115200, // DSMR 4.2
   databits: 8,
-  // parity: "none"
-}
+  parity: "none",
+};
 
-// const TOPIC_ROOT = "loef6";
-
-// TODO use broker URL from env
-// const mqttBrokerUrl = "mqtt://test.mosquitto.org";
+// topic root for my house
+const TOPIC_ROOT = "loef6";
+// topic root for the group of smart energy meter apps
+const DOMAIN_ROOT = "SEM";
 
 const smartMeter = new SmartMeter(smartMeterOptions);
 
-// const mqttClient = mqtt.connect(mqttBrokerUrl);
+const mqttClient = mqtt.connect(process.env.MQTT_BROKER_URL);
 
 smartMeter.on("connected", () => {
   console.log("connected to smart meter");
-  // mqttClient.publish(`${TOPIC_ROOT}/presence`, "smart meter is online");
+  mqttClient.publish(`${TOPIC_ROOT}/presence`, "smart meter is online");
 });
-smartMeter.on("telegram", telegram => {
-  console.log("telegram received", telegram);
-  // mqttClient.publish(`${TOPIC_ROOT}/SEM/telegram`, telegram);
+smartMeter.on("telegram", (telegram) => {
+  mqttClient.publish(`${TOPIC_ROOT}/${DOMAIN_ROOT}/telegram`, telegram);
 });
-smartMeter.on('disconnected', () => {
-  console.log("smart meter is offline");
-})
+smartMeter.on("disconnected", () => {
+  console.log("... SEMP disconnected from smart meter");
+});
 
 console.log("SEMP connecting to smart meter ...");
 smartMeter.connect();
